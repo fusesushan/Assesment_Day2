5c9e0cbe7f9bb70de4c8450e70ec00cdcb74e201
fusesushan <sushan.kattel@fusemachines.com>
2023-08-03 16:16:06 +0545

    feat: Adding functions that calculates sum of any number of groceries

---
+++ kwargs2.py
@@ -0,0 +1,16 @@
+'''
+[**kwargs] Write a Python function calculate_total_cost that calculates the total
+cost of items purchased from a store. The function should accept multiple keyword
+arguments, where the key is the item name, and the value is the item's price. The
+function should return the total cost of all items.
+'''
+
+def calculate_total_cost(**kwargs):
+    '''function implementing kwargs tat gives total cost of items purchased'''
+    price = 0
+    for _, v in kwargs.items():
+        price += v
+    return price
+
+print(calculate_total_cost.__doc__)
+print(calculate_total_cost(potato=100, Dettol=560, Basmati=2790))
